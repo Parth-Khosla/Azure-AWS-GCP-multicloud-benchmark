a62ba751b7752a89b4dcdfdfb5637f8f5c2e7475
Parth-Khosla <khosla.parth@gmail.com>
2025-10-16 10:59:54 +0530

    Remove squashed GCP-VM-Benchmark subtree

--- GCP-VM-Benchmark/README.md
+++
@@ -1,2 +0,0 @@
-# GCP-VM-Benchmark
-This Repository contains needed files to automate bench-marking of time taken for VM deployment on GCP
--- GCP-VM-Benchmark/login.sh
+++
@@ -1,8 +0,0 @@
-
-echo "🔑 Logging into gcloud CLI..."
-gcloud auth login --no-launch-browser "$@"
-
-echo "🔑 Setting up application default credentials..."
-gcloud auth application-default login
-
-echo "✅ Both CLI and ADC authentication completed."
